a65fe3b24ee8ec1788e33d68be478ae34d786697
Kevin Viglucci <kviglucci@gmail.com>
2020-04-04 22:46:25 -0500

    add auth and passport scaffolding

---
+++ identity-server/resources/views/home.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div id="app">
+        <div class="row justify-content-center mb-2">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        You are logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center mb-2">
+            <div class="col-md-8">
+                <passport-clients></passport-clients>
+            </div>
+        </div>
+
+        <div class="row justify-content-center mb-2">
+            <div class="col-md-8">
+                <passport-authorized-clients></passport-authorized-clients>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <passport-personal-access-tokens></passport-personal-access-tokens>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ identity-server/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Hello World</h1>
+    </div>
+@endsection
